faad908b13fab4daeb4bbbb360aaef76716e8e44
hey2017 <mottaghi.navid@gmail.com>
2021-08-31 14:36:00 -0400

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -8,35 +8,29 @@ from sklearn.covariance import EllipticEnvelope
 from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import LinearRegression
 
-os.chdir(directory)
+os.chdir(Directory)
 
-#%%
 df2 = pd.read_csv('Housing_Data.csv')
-
-#%%
-#------------Isolationforest
 
 data = df2.values
 
-#%%
 x, y = data[:,:-1], data[:,-1]
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.33, random_state = 1)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
 
 
-#%%
-contamination_list = list(np.linspace(0,0.5,num=100))
-
+nu_list = list(np.linspace(0.00001,0.99999,num=100))
 #%%
 result_lst =[]
 
-for i in contamination_list:
+for i in nu_list:
+    print(i)
     x_train2 = 0
     y_train2 = 0
     y_pred_linear = 0
     y_pred2_linear= 0
     
-    model = EllipticEnvelope (contamination = i)
+    model = OneClassSVM(nu = i)
     y_pred = model.fit_predict(x_train)
     tag = y_pred != -1
     x_train2, y_train2 = x_train[tag,:], y_train[tag]
@@ -45,17 +39,17 @@ for i in contamination_list:
     #before detecting the outliers
     linear_model.fit(x_train, y_train)
     y_pred_linear = linear_model.predict(x_test)
-    
+    #after detecting the outliers
     linear_model.fit(x_train2, y_train2)
     y_pred2_linear = linear_model.predict(x_test)
     
     result_lst.append((i, mean_absolute_error(y_test,y_pred_linear), mean_absolute_error(y_test,y_pred2_linear)))
 #%%
 result = pd.DataFrame(result_lst)
-result.columns = ['contamination value', 'mae before fit', 'mae after fit']
+result.columns = ['nu', 'mae before fit', 'mae after fit']
 result = result.sort_values(by='mae after fit', ascending = True)
 
-print('The best contamination value is:\n ', result.iloc[0])
+print('The nu is:\n ', result.iloc[0])
 
 #%%
 mae_after = []
@@ -63,8 +57,8 @@ contamination_val = []
 
 for i in range(len(result_lst)):
     mae_after.append(result_lst[i][2])
-    contamination_val.append(result_lst[i][0])
+    nu.append(result_lst[i][0])
     
-plt.plot(contamination_val, mae_after,'--*')
+plt.plot(nu, mae_after,'--*')
 plt.ylabel('absolute mean square after fit')
-plt.xlabel('contamination value')
+plt.xlabel('nu value')
